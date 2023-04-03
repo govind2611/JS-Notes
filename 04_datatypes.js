e7e30b330e140c2fb7155c5000061feccefa4e42
Govindsing Chavan <123160733+govind2611@users.noreply.github.com>
2023-04-03 08:43:29 +0530

    datatype

---
+++ 04_datatypes.js
@@ -0,0 +1,48 @@
+/*   
+ 
+    JavaScript has 8 Datatypes
+        1. String
+        2. Number
+        3. Bigint
+        4. Boolean
+        5. Undefined
+        6. Null
+        7. Symbol
+        8. Object
+
+    The Object Datatype
+    The object data type can contain:
+
+        1. An object
+        2. An array
+        3. A date
+*/
+
+// Numbers:
+let length = 16;
+let weight = 7.5;
+
+// Strings:
+let color = "Yellow";
+let lastName = "Johnson";
+
+// Booleans
+let x = true;
+let y = false;
+
+// Object:
+const person = { firstName: "John", lastName: "Doe" };
+
+// Array object:
+const cars = ["Saab", "Volvo", "BMW"];
+
+// Date object:
+const date = new Date("2022-03-25");
+
+/*
+    Note:- 
+        When adding a number and a string, JavaScript will treat the number as a string.
+        JavaScript evaluates expressions from left to right.
+        JavaScript has dynamic types.This means that the same variable can be used to hold different data types
+
+*/
