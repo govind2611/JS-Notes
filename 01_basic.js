f5b435b47508a5a28fba6526c2a33515e636856c
Govindsing Chavan <123160733+govind2611@users.noreply.github.com>
2023-04-03 08:42:17 +0530

    basic of js

---
+++ 01_basic.js
@@ -0,0 +1,38 @@
+// JavaScript is one of the 3 languages all web developers must learn:
+
+//    1. HTML to define the content of web pages
+
+//    2. CSS to specify the layout of web pages
+
+//    3. JavaScript to program the behavior of web pages
+
+/* keypoints:- 
+    1. JavaScript accepts both double and single quotes:
+    2. JavaScript was invented by Brendan Eich in 1995, and became an ECMA standard in 1997.
+    3. ECMA-262 is the official name of the standard. ECMAScript is the official name of the language
+    4.Semicolons separate JavaScript statements.
+    Add a semicolon at the end of each executable statement.
+    5.JavaScript ignores multiple spaces. You can add white space to your script to make it more readable.
+
+
+*/
+
+/*
+JavaScript Can Change HTML Attribute Values
+
+The example below "finds" an HTML element (with id="demo"), and changes the element content (innerHTML) to "Hello JavaScript":
+*/
+document.getElementById("demo").innerHTML = "Hello JavaScript";
+ 
+
+
+/*
+JavaScript Can Change HTML Styles (CSS)
+Changing the style of an HTML element, is a variant of changing an HTML attribute:
+*/ 
+
+document.getElementById("demo").style.fontSize = "35px";
+
+//  In HTML, JavaScript code is inserted between <script> and </script> tags.
+//  Scripts can be placed in the <body>, or in the <head> section of an HTML page, or in both.
+//  and also can be added a external js file
