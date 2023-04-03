d6eec27165478b0340443ed5f4e72af66067d080
Govindsing Chavan <123160733+govind2611@users.noreply.github.com>
2023-04-03 08:48:34 +0530

    assync programming

---
+++ 19_assyncProg.js
@@ -0,0 +1,16 @@
+/*
+    Synchronous programming
+    
+    Synchronous programming follows a strict set of sequences. When the code runs in a synchronous program, it will follow each step of an algorithm. It does so in order and will wait for the present operation to finish before continuing on to the next.
+*/
+
+/*
+    Asynchronous programming
+    
+    Asynchronous cake baking, by contrast, allows multiple people to be working on the task at once. One person can gather and measure the ingredients while another person begins mixing the ingredients together. Asynchronous programming allows multiple processes to be started, lets the processes do their work, and when their job is finished, it gets the result and puts it through the steps.
+
+    Asynchronous is not always the best way to go. Asynchronous programs add more complexity and make the code more unreadable. Young programmers will often use async functions too much because they think it acts as a safeguard to ensure their code works at run-time. A general rule for when to use async functions:
+
+    Good for: Tasks that may take a while; high iteration.
+    Bad for: Simplicity.
+*/
