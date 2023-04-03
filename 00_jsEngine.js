fd9e5a6dd162464b9c09229e063701fb9097f7e1
Govindsing Chavan <123160733+govind2611@users.noreply.github.com>
2023-04-03 08:41:50 +0530

    js engine and runtime stack

---
+++ 00_jsEngine.js
@@ -0,0 +1,10 @@
+// To run the js code we always need a JS engine.A JavaScript engine is a software component that executes JavaScript code.
+// js engine is present in javascript runtime environment
+
+// JRE=== BROWSER (every browser has JRE)
+
+// List of JavaScript Engines:
+     // Google Chrome -->V8
+    // Edge (Internet Explorer)--> Chakra
+    // Mozilla Firefox --> Spider Monkey
+     //Safari --> Javascript Core Webkit
