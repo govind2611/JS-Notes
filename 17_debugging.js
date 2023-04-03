dfd7d33acb83197633a31b2b33eaa7b731d74be8
Govindsing Chavan <123160733+govind2611@users.noreply.github.com>
2023-04-03 08:47:39 +0530

    debugging

---
+++ 17_debugging.js
@@ -0,0 +1,27 @@
+/*
+Debugging is not easy. But fortunately, all modern browsers have a built-in JavaScript debugger.
+Built-in debuggers can be turned on and off, forcing errors to be reported to the user.
+With a debugger, you can also set breakpoints (places where code execution can be stopped), and examine variables while the code is executing.
+Normally (otherwise follow the steps at the bottom of this page), you activate debugging in your browser with the F12 key, and select "Console" in the debugger menu.
+*/
+
+
+// Setting Breakpoints
+/*
+    In the debugger window, you can set breakpoints in the JavaScript code.
+    At each breakpoint, JavaScript will stop executing, and let you examine JavaScript values.
+    After examining values, you can resume the execution of code (typically with a play button).
+*/
+
+// The debugger Keyword
+/* 
+    The debugger keyword stops the execution of JavaScript, and calls (if available) the debugging function.
+    This has the same function as setting a breakpoint in the debugger.
+    If no debugging is available, the debugger statement has no effect.
+*/
+
+
+/* 
+    Debugging is the process of testing, finding, and reducing bugs (errors) in computer programs.
+    The first known computer bug was a real bug (an insect) stuck in the electronics. 
+*/
